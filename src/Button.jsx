2df1fc2cba9a2b0b9033fe4e70757b5730d2be12
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-12 18:59:12 +0530

    project 2 done

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Button(){
     return (
         <div className="button-section">
-            <button className="email-el">Email✉️</button>
-            <button className="link-el">LinkedIn <img className="img2-el" src="linkedin (1).png" /></button>
+            <a href="mailto:singhgaganbtp@gmail.com"><button className="email-el">Email✉️</button></a>
+            <a href="https://www.linkedin.com/in/gagan-chaudhary-443174216/"><button className="link-el">LinkedIn <img className="img2-el" src="linkedin (1).png" /></button></a>
         </div>
     )
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer(){
     return (
         <footer className="foot-el">
-            <img className="twitter-el" src="twitter.png" />
-            <img className="github-el" src="github.png" />
+            <a href="https://twitter.com/GaganCh88472243?t=zv6kxbke1RgMP8Z4ebXEwQ&s=08 "><img className="twitter-el" src="twitter.png" /></a>
+            <a href="https://github.com/GaganChaudhary6378"><img className="github-el" src="github.png" /></a>
         </footer>
     )
 }
